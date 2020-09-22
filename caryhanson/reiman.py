c8ffc2a34d08735440024f544aaa124e34e57f67
Matt Landreman <mattland@umd.edu>
2020-09-21 20:36:56 -0400

    Now computing derivatives of the Reiman field

--- caryhanson/reiman.py
+++ caryhanson/reiman.py
@@ -1,11 +1,50 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3AOAA
 
 """
 This module contains a class for the Reiman-Greensides model magnetic field.
+For derivation of expressions here, see
+20200917-05 Reiman field.docx
 """
 
+# These next 2 lines tell jax to use double precision:
+from jax.config import config
+config.update("jax_enable_x64", True)
+
+#import jax.numpy as jnp
+import numpy as jnp
 import numpy as np
+#from jax import grad, jit
 from .field import Field
+
+#@jit
+def B_func(R_phi_Z, iotaj, eps, B0, R0, ms):
+    """
+    This function computes B in a form that is convenient for automatic differentiation.
+    The arguments iotaj and eps should be numpy arrays.
+    """
+    R = R_phi_Z[0]
+    phi = R_phi_Z[1]
+    Z = R_phi_Z[2]
+    theta = jnp.arctan2(Z, R - R0)
+
+    iotaja = np.array(iotaj)
+    epsa = np.array(eps)
+    msa = np.array(ms)
+
+    r = jnp.sqrt(Z * Z + (R - R0) * (R - R0))
+    psi = 0.5 * B0 * r * r
+    jarr = jnp.arange(len(iotaj)) + 1
+
+    temp = ms * epsa * (r ** (msa - 2))
+    Q0 = -jnp.sum(temp * jnp.cos(msa * theta - phi)) + B0 * jnp.sum(iotaja * jarr * (psi ** (jarr - 1)))
+    Q1 =  jnp.sum(temp * jnp.sin(msa * theta - phi))
+
+    BR = (Z * Q0 + (R - R0) * Q1) / R
+    Bphi = -B0
+    BZ = (Z * Q1 - (R - R0) * Q0) / R
+    return np.array([BR, Bphi, BZ])
+
+#mygrad = grad(B_func)
 
 class ReimanField(Field):
     """
@@ -19,36 +58,96 @@ class ReimanField(Field):
         self.iotaj = np.array(iotaj)
         self.eps = np.array(eps)
         self.ms = np.array(ms)
-        self.nfp = 1
+        self.nfp = 1        
 
-    def B_func(self, R_phi_Z, iotaj, eps):
-        """
-        This function computes B in a form that is convenient for automatic differentiation.
-        The arguments iotaj and eps should be numpy arrays.
-        """
-        R = R_phi_Z[0]
-        phi = R_phi_Z[1]
-        Z = R_phi_Z[2]
-        theta = np.arctan2(Z, R - self.R0)
-        
-        r = np.sqrt(Z * Z + (R - self.R0) * (R - self.R0))
-        psi = 0.5 * self.B0 * r * r
-        jarr = np.arange(len(self.iotaj)) + 1
-
-        temp = self.ms * eps * (r ** (self.ms - 2))
-        Q0 = np.sum(temp * np.cos(self.ms * theta - phi)) + self.B0 * np.sum(iotaj * jarr * (psi ** (jarr - 1)))
-        Q1 = np.sum(temp * np.sin(self.ms * theta - phi))
-
-        BR = (Z * Q0 + (R - self.R0) * Q1) / R
-        Bphi = -self.B0
-        BZ = (Z * Q1 - (R - self.R0) * Q0) / R
-        return BR, Bphi, BZ
-        
-        
     def BR_Bphi_BZ(self, R, phi, Z):
         """
         Return the cylindrical components of the magnetic field at a specified point.
         This subroutine only works for single points as arguments.
         """
 
-        return self.B_func([R, phi, Z], self.iotaj, self.eps)
+        return B_func([R, phi, Z], self.iotaj, self.eps, self.B0, self.R0, self.ms)
+
+    def grad_B(self, R, phi, Z):
+        """
+        Return the derivatives with respect to (R, phi, Z) of the cylindrical components of B.
+        """
+        R0 = self.R0
+        B0 = self.B0
+        theta = np.arctan2(Z, R - R0)
+
+        r = np.sqrt(Z * Z + (R - R0) * (R - R0))
+        psi = 0.5 * B0 * r * r
+        jarr = np.arange(len(self.iotaj)) + 1
+
+        temp = self.ms * self.eps * (r ** (self.ms - 2))
+        sines = np.sin(self.ms * theta - phi)
+        coses = np.cos(self.ms * theta - phi)
+        cos_terms = np.sum(temp * coses)
+        Q0 = -cos_terms + B0 * np.sum(self.iotaj * jarr * (psi ** (jarr - 1)))
+        Q1 = np.sum(temp * sines)
+
+        d_Q0_d_phi = -Q1
+        d_Q1_d_phi = -cos_terms
+        
+        temp = self.ms * self.eps * (r ** (self.ms - 4))
+        m2_cos_terms = np.sum(temp * (self.ms - 2) * coses)
+        m_cos_terms  = np.sum(temp * (self.ms    ) * coses)
+        m2_sin_terms = np.sum(temp * (self.ms - 2) * sines)
+        m_sin_terms  = np.sum(temp * (self.ms    ) * sines)
+
+        iota_terms = self.iotaj * jarr * (jarr - 1) * (psi ** (jarr - 2))
+        if len(self.iotaj) > 0:
+            iota_terms[0] = 0
+        #if len(self.iotaj) > 1:
+        #    iota_terms[1] = 0
+        iota_terms_sum = np.sum(iota_terms)
+        print('iota_terms_sum: ', iota_terms_sum)
+        d_Q1_d_R = (R - R0) * m2_sin_terms - Z * m_cos_terms
+        d_Q1_d_Z = Z * m2_sin_terms + (R - R0) * m_cos_terms
+        d_Q0_d_R = -((R - R0) * m2_cos_terms + Z * m_sin_terms) + B0 * B0 * (R - R0) * iota_terms_sum
+        d_Q0_d_Z = -(Z * m2_cos_terms - (R - R0) * m_sin_terms) + B0 * B0 * Z * iota_terms_sum
+        
+        print('d_Q0_d_R an:', d_Q0_d_R)
+        print('d_Q1_d_R an:', d_Q1_d_R)
+        print('d_Q0_d_Z an:', d_Q0_d_Z)
+        print('d_Q1_d_Z an:', d_Q1_d_Z)
+        #BR = (Z * Q0 + (R - R0) * Q1) / R
+        #Bphi = -B0
+        #BZ = (Z * Q1 - (R - R0) * Q0) / R
+
+        d_BR_d_R = -Z * Q0 / (R * R) + (Z / R) * d_Q0_d_R + R0 * Q1 / (R * R) + (R - R0) * d_Q1_d_R / R
+        d_BR_d_phi = (Z * d_Q0_d_phi + (R - R0) * d_Q1_d_phi) / R
+        d_BR_d_Z = (Q0 + Z * d_Q0_d_Z + (R - R0) * d_Q1_d_Z) / R
+        
+        d_Bphi_d_R = 0
+        d_Bphi_d_phi = 0
+        d_Bphi_d_Z = 0
+
+        d_BZ_d_R = -R0 * Q0 / (R * R) - (R - R0) * d_Q0_d_R / R - Z * Q1 / (R * R) + Z * d_Q1_d_R / R
+        d_BZ_d_phi = (-(R - R0) * d_Q0_d_phi + Z * d_Q1_d_phi) / R
+        d_BZ_d_Z = (-(R - R0) * d_Q0_d_Z + Q1 + Z * d_Q1_d_Z) / R
+        
+        return np.array([[d_BR_d_R, d_BR_d_phi, d_BR_d_Z],
+                         [d_Bphi_d_R, d_Bphi_d_phi, d_Bphi_d_Z],
+                         [d_BZ_d_R, d_BZ_d_phi, d_BZ_d_Z]])
+
+    def Q0_Q1(self, R, phi, Z):
+        """
+        This subroutine is only for testing. It returns the intermediate expressions Q0 and Q1.
+        """
+        R0 = self.R0
+        B0 = self.B0
+        theta = np.arctan2(Z, R - R0)
+
+        r = np.sqrt(Z * Z + (R - R0) * (R - R0))
+        psi = 0.5 * B0 * r * r
+        jarr = np.arange(len(self.iotaj)) + 1
+
+        temp = self.ms * self.eps * (r ** (self.ms - 2))
+        sines = np.sin(self.ms * theta - phi)
+        coses = np.cos(self.ms * theta - phi)
+        cos_terms = np.sum(temp * coses)
+        Q0 = -cos_terms + B0 * np.sum(self.iotaj * jarr * (psi ** (jarr - 1)))
+        Q1 = np.sum(temp * sines)
+        return Q0, Q1
--- caryhanson/tests/test_reiman.py
+++ caryhanson/tests/test_reiman.py
@@ -12,9 +12,11 @@ class ReimanFieldTests(unittest.TestCase):
         """
         rf = ReimanField()
         BR, Bphi, BZ = rf.BR_Bphi_BZ(1.1, 0.7, 0.2)
-        BR_true = 0.03074843023437838
+        #BR_true = 0.03074843023437838
+        BR_true = 0.03069701258456715
         Bphi_true = -1
-        BZ_true = -0.015385593411689104
+        BZ_true = -0.015359884586783487
+        #BZ_true = -0.015385593411689104
         places = 13
         print("Reiman field: diff in BR={}, Bphi={}, BZ={}".format(
             BR - BR_true, Bphi - Bphi_true, BZ - BZ_true))
@@ -22,7 +24,98 @@ class ReimanFieldTests(unittest.TestCase):
         self.assertAlmostEqual(Bphi, Bphi_true, places=places)
         self.assertAlmostEqual(BZ, BZ_true, places=places)
 
+    def test_derivatives(self):
+        """
+        Test the derivatives of B.
+        """
+        #rf = ReimanField(iotaj=[0])
+        for j in range(30):
+            niota = np.random.randint(1,4) # 1, 2, or 3
+            iotaj = np.random.rand(niota) * 2 - 1
+            B0 = np.random.rand() + 0.5
+            R0 = np.random.rand() + 0.5
+            nm = np.random.randint(1,4) # 1, 2, or 3
+            ms = np.random.randint(1, 10, size=(nm,))
+            eps = (np.random.rand(nm) - 0.5) * 0.1
+            print('iotaj: ', iotaj)
+            print('ms: ', ms)
+            print('eps: ', eps)
+            
+            rf = ReimanField(iotaj=iotaj, B0=B0, R0=R0, ms=ms, eps=eps)
+            # Pick a random location:
+            R = np.random.rand() + 0.5
+            phi = np.random.rand() * 10 - 5
+            Z = np.random.rand() * 4 - 2
+            #R = 1.1
+            #phi = 0
+            #Z = 0.0
 
-        
+            # Evaluate finite difference derivatives
+            delta = 1e-6
+            delta2 = delta * 2
+            
+            BRp, Bphip, BZp = rf.BR_Bphi_BZ(R + delta, phi, Z)
+            BRm, Bphim, BZm = rf.BR_Bphi_BZ(R - delta, phi, Z)
+            Q0p, Q1p = rf.Q0_Q1(R + delta, phi, Z)
+            Q0m, Q1m = rf.Q0_Q1(R - delta, phi, Z)
+            
+            d_BR_d_R_fd = (BRp - BRm) / delta2
+            d_Bphi_d_R_fd = (Bphip - Bphim) / delta2
+            d_BZ_d_R_fd = (BZp - BZm) / delta2
+            d_Q0_d_R = (Q0p - Q0m) / delta2
+            d_Q1_d_R = (Q1p - Q1m) / delta2
+            print('d_Q0_d_R FD:', d_Q0_d_R)
+            print('d_Q1_d_R FD:', d_Q1_d_R)
+            
+            BRp, Bphip, BZp = rf.BR_Bphi_BZ(R, phi + delta, Z)
+            BRm, Bphim, BZm = rf.BR_Bphi_BZ(R, phi - delta, Z)
+            Q0p, Q1p = rf.Q0_Q1(R, phi + delta, Z)
+            Q0m, Q1m = rf.Q0_Q1(R, phi - delta, Z)
+
+            d_BR_d_phi_fd = (BRp - BRm) / delta2
+            d_Bphi_d_phi_fd = (Bphip - Bphim) / delta2
+            d_BZ_d_phi_fd = (BZp - BZm) / delta2
+            d_Q0_d_phi = (Q0p - Q0m) / delta2
+            d_Q1_d_phi = (Q1p - Q1m) / delta2
+
+            BRp, Bphip, BZp = rf.BR_Bphi_BZ(R, phi, Z + delta)
+            BRm, Bphim, BZm = rf.BR_Bphi_BZ(R, phi, Z - delta)
+            Q0p, Q1p = rf.Q0_Q1(R, phi, Z + delta)
+            Q0m, Q1m = rf.Q0_Q1(R, phi, Z - delta)
+
+            d_BR_d_Z_fd = (BRp - BRm) / delta2
+            d_Bphi_d_Z_fd = (Bphip - Bphim) / delta2
+            d_BZ_d_Z_fd = (BZp - BZm) / delta2
+            d_Q0_d_Z = (Q0p - Q0m) / delta2
+            d_Q1_d_Z = (Q1p - Q1m) / delta2
+            print('d_Q0_d_Z FD:', d_Q0_d_Z)
+            print('d_Q1_d_Z FD:', d_Q1_d_Z)
+
+            print('Finite difference derivatives:')
+            grad_B_fd = np.array([[d_BR_d_R_fd, d_BR_d_phi_fd, d_BR_d_Z_fd],
+                         [d_Bphi_d_R_fd, d_Bphi_d_phi_fd, d_Bphi_d_Z_fd],
+                         [d_BZ_d_R_fd, d_BZ_d_phi_fd, d_BZ_d_Z_fd]])
+            print(grad_B_fd)
+            # Evaluate the analytic derivatives:
+            grad_B = rf.grad_B(R, phi, Z)
+            print('Analytic derivatives:')
+            print(grad_B)
+
+            print('Differences:')
+            print(grad_B - grad_B_fd)
+
+            places = 6
+            self.assertAlmostEqual(d_BR_d_R_fd, grad_B[0,0], places=places)
+            self.assertAlmostEqual(d_BR_d_phi_fd, grad_B[0,1], places=places)
+            self.assertAlmostEqual(d_BR_d_Z_fd, grad_B[0,2], places=places)
+            
+            self.assertAlmostEqual(d_Bphi_d_R_fd, grad_B[1,0], places=places)
+            self.assertAlmostEqual(d_Bphi_d_phi_fd, grad_B[1,1], places=places)
+            self.assertAlmostEqual(d_Bphi_d_Z_fd, grad_B[1,2], places=places)
+            
+            self.assertAlmostEqual(d_BZ_d_R_fd, grad_B[2,0], places=places)
+            self.assertAlmostEqual(d_BZ_d_phi_fd, grad_B[2,1], places=places)
+            self.assertAlmostEqual(d_BZ_d_Z_fd, grad_B[2,2], places=places)
+            
 if __name__ == "__main__":
     unittest.main()
