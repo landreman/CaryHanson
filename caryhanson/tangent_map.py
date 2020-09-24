6f16f8155af81846a21584720a379698b41b6f75
Matt Landreman <mattland@umd.edu>
2020-09-24 12:47:46 -0400

    Refactored tangent_map to save the single-period maps

--- caryhanson/tangent_map.py
+++ caryhanson/tangent_map.py
@@ -44,7 +44,7 @@ class Struct:
     """
     pass
 
-def tangent_map(field, periods=1, R0=1, Z0=0, rtol=1e-6, atol=1e-9):
+def tangent_map(field, pfl, rtol=1e-6, atol=1e-9):
     """
     Computes the full-orbit tangent map for a periodic fieldline, as well
     as quantities derived from the map like the rotational transform and Greene's residue.
@@ -58,75 +58,126 @@ def tangent_map(field, periods=1, R0=1, Z0=0, rtol=1e-6, atol=1e-9):
 
     R0 and Z0 should each be a float, previously computed using periodic_field_line().
     """
+
+    periods = pfl.periods # shorthand
     
-    phimax = periods * 2 * np.pi / field.nfp
-    t_span = (0, phimax)
-
-    # The state vector has 6 unknowns: R, Z, and the 4 elements of the U matrix.
-    x0 = [R0, Z0, 1, 0, 0, 1]
-
     def tangent_map_integrand_wrapper(t, y):
         return tangent_map_integrand(t, y, field)
-    
-    soln = solve_ivp(tangent_map_integrand_wrapper, t_span, x0, rtol=rtol, atol=atol)
 
-    print('# of function evals: ', soln.nfev)
-    
-    # Make sure we got to the end:
-    assert np.abs(soln.t[-1] - phimax) < 1e-13
-    
-    R = soln.y[0, :]
-    Z = soln.y[1, :]
-    # Make sure field line was indeed periodic:
-    print('R(end) - R(0): ', R[-1] - R0)
-    print('Z(end) - Z(0): ', Z[-1] - Z0)
+    single_period_tangent_maps = []
+    R_end = np.roll(pfl.R_k, -1)
+    Z_end = np.roll(pfl.Z_k, -1)
+    print('R_k:  ', pfl.R_k)
+    print('R_end:', R_end)
+    print('Z_k:  ', pfl.Z_k)
+    print('Z_end:', Z_end)
+    for j in range(periods):
+        print('---- Computing tangent map for period {} ----'.format(j))
+        phimax = 2 * np.pi / field.nfp
+        t_span = (0, phimax)
+        # Initialize using (R,Z) from the spectral approach, since it
+        # may be more accurate than the initial-value calculation
+        # here.
+        R0 = pfl.R_k[j]
+        Z0 = pfl.Z_k[j]
+        
+        # The state vector has 6 unknowns: R, Z, and the 4 elements of the U matrix.
+        x0 = [R0, Z0, 1, 0, 0, 1]
 
-    tol = 1e-5
-    if np.abs(R[-1] - R0) > tol or np.abs(Z[-1] - Z0) > tol:
-        raise RuntimeError('Field line is not closed. Values of R0 and Z0 provided must have been incorrect')
+        soln = solve_ivp(tangent_map_integrand_wrapper, t_span, x0, rtol=rtol, atol=atol)
 
-    # Form the full-orbit tangent map:
-    M = np.array([[soln.y[2, -1], soln.y[3, -1]],
-                  [soln.y[4, -1], soln.y[5, -1]]])
+        print('# of function evals: ', soln.nfev)
 
-    print('M: ', M)
-    det = np.linalg.det(M)
-    print('determinant: ', det)
-    if np.abs(det - 1) > 1e-4:
-        raise RuntimeError('Determinant of tangent map is not close to 1!')
-    
-    eigvals, eigvects = np.linalg.eig(M)
-    print('eigvals: ', eigvals)
-    print('eigvects: ', eigvects)
+        # Make sure we got to the end:
+        assert np.abs(soln.t[-1] - phimax) < 1e-13
 
-    iota_per_period = np.angle(eigvals[0]) / (2 * np.pi)
-    iota = iota_per_period * field.nfp
-    residue = 0.25 * (2 - np.trace(M))
-    print('iota per period: {},  total iota: {},  residue: {}'.format(iota_per_period, iota, residue))
+        R = soln.y[0, :]
+        Z = soln.y[1, :]
+        # Make sure field line is close to the result from periodic_field_line:
+        print('R(end) - R0(k+1): ', R[-1] - R_end[j])
+        print('Z(end) - Z0(k+1): ', Z[-1] - Z_end[j])
 
+        tol = 1e-5
+        if np.abs(R[-1] - R_end[j]) > tol or np.abs(Z[-1] - Z_end[j]) > tol:
+            raise RuntimeError('Field line is not closed. Values of R0 and Z0 provided must have been incorrect')
+
+        # Form the single-period tangent map:
+        S = np.array([[soln.y[2, -1], soln.y[3, -1]],
+                      [soln.y[4, -1], soln.y[5, -1]]])
+        
+        print('S: ', S)
+        det = np.linalg.det(S)
+        print('determinant of S: ', det)
+
+        single_period_tangent_maps.append(S)
+
+    full_orbit_tangent_maps = []
+    eigvals = []
+    eigvects = []
+    iotas = []
+    iotas_per_period = []
+    residues = []
+    W_eigvals = []
+    W_eigvects = []
+    epars = []
+    eperps = []
     sigma = np.array([[0, 1], [-1, 0]])
-    tempmat = np.matmul(sigma, M)
-    W = 0.5 * (tempmat + tempmat.transpose())
-    W_eigvals, W_eigvects = np.linalg.eig(W)
-    print('W_eigvals: ', W_eigvals)
-    print('W_eigvects: ', W_eigvects)
-    # The larger eigenvalue corresponds to eperp
-    if np.abs(W_eigvals[0]) > np.abs(W_eigvals[1]):
-        eperp = W_eigvects[:,0]
-        epar = W_eigvects[:,1]
-    else:
-        eperp = W_eigvects[:,1]
-        epar = W_eigvects[:,0]
-    
+    for j in range(periods):
+        print('---- Period {} ----'.format(j))
+        # Multiple all the single-period tangent maps together to get the full-orbit tangent map:
+        M = single_period_tangent_maps[j]
+        for k in range(1, periods):
+            M = np.matmul(single_period_tangent_maps[np.mod(j + k, periods)], M)
+            #M = np.matmul(M, single_period_tangent_maps[np.mod(j + k, periods)])
+
+        print("M:", M)
+        det = np.linalg.det(M)
+        print('determinant of M: ', det)
+        if np.abs(det - 1) > 1e-4:
+            raise RuntimeError('Determinant of tangent map is not close to 1!')
+
+        eigvals_j, eigvects_j = np.linalg.eig(M)
+        print('eigvals: ', eigvals_j)
+        print('eigvects: ', eigvects_j)
+
+        iota_per_period = np.angle(eigvals_j[0]) / (2 * np.pi)
+        iota = iota_per_period * field.nfp
+        residue = 0.25 * (2 - np.trace(M))
+        print('iota per period: {},  total iota: {},  residue: {}'.format(iota_per_period, iota, residue))
+
+        tempmat = np.matmul(sigma, M)
+        W = 0.5 * (tempmat + tempmat.transpose())
+        W_eigvals_j, W_eigvects_j = np.linalg.eig(W)
+        print('W_eigvals: ', W_eigvals_j)
+        print('W_eigvects: ', W_eigvects_j)
+        # The larger eigenvalue corresponds to eperp
+        if np.abs(W_eigvals_j[0]) > np.abs(W_eigvals_j[1]):
+            eperp = W_eigvects_j[:,0]
+            epar = W_eigvects_j[:,1]
+        else:
+            eperp = W_eigvects_j[:,1]
+            epar = W_eigvects_j[:,0]
+
+        full_orbit_tangent_maps.append(M)
+        eigvals.append(eigvals_j)
+        eigvects.append(eigvects_j)
+        iotas_per_period.append(iota_per_period)
+        iotas.append(iota)
+        residues.append(residue)
+        W_eigvals.append(W_eigvals_j)
+        W_eigvects.append(W_eigvects_j)
+        epars.append(epar)
+        eperps.append(eperp)
     
     results = Struct()
-    results.mat = M
+    results.single_period_tangent_maps = single_period_tangent_maps
+    results.full_orbit_tangent_maps = full_orbit_tangent_maps
     results.eigvals = eigvals
     results.eigvects = eigvects
     results.iota_per_period = iota_per_period
     results.iota = iota
     results.residue = residue
-    results.epar = epar
-    results.eperp = eperp
+    results.epars = epars
+    results.eperps = eperps
     
     return results
--- caryhanson/tests/test_tangent_map.py
+++ caryhanson/tests/test_tangent_map.py
@@ -16,13 +16,16 @@ class TangentMapTests(unittest.TestCase):
         """
 
         field = HelicalCoil()
-        tm = tangent_map(field, R0=0.98343287162797)
+        pfl = periodic_field_line(field, 31, periods=1, R0=0.983)
+        #tm = tangent_map(field, R0=0.98343287162797)
+        tm = tangent_map(field, pfl)
         places = 8
         self.assertAlmostEqual(tm.iota_per_period, 0.11287146743203362, places=places)
         self.assertAlmostEqual(tm.iota, 0.5643573371601681, places=places)
         M = np.array([[ 0.75888781375709 ,  1.37679244594353 ],
                       [-0.308026489831821,  0.758887765057904]])
-        np.testing.assert_allclose(tm.mat, M, atol=1.0e-6)
+        np.testing.assert_allclose(tm.full_orbit_tangent_maps[0], M, atol=1.0e-6)
+        np.testing.assert_allclose(tm.single_period_tangent_maps[0], M, atol=1.0e-6)
         self.assertAlmostEqual(tm.residue, 0.12055610529625149, places=places)
         
     def test_reiman_axis(self):
@@ -32,13 +35,16 @@ class TangentMapTests(unittest.TestCase):
         """
 
         field = ReimanField()
-        tm = tangent_map(field, R0=1.0, rtol=1e-12, atol=1e-12)
+        pfl = periodic_field_line(field, 11, periods=1, R0=1.0)
+        #tm = tangent_map(field, R0=1.0, rtol=1e-12, atol=1e-12)
+        tm = tangent_map(field, pfl, rtol=1e-12, atol=1e-12)
         places = 10
         self.assertAlmostEqual(tm.iota_per_period, 0.15, places=places)
         self.assertAlmostEqual(tm.iota, 0.15, places=places)
         M = np.array([[ 0.587785252292183, -0.809016994374573],
                       [ 0.809016994374573, 0.587785252292183]])
-        np.testing.assert_allclose(tm.mat, M, atol=1.0e-6)
+        np.testing.assert_allclose(tm.full_orbit_tangent_maps[0], M, atol=1.0e-6)
+        np.testing.assert_allclose(tm.single_period_tangent_maps[0], M, atol=1.0e-6)
         self.assertAlmostEqual(tm.residue, 0.20610737385390826, places=places)
         
     def test_reiman_Opoint(self):
@@ -48,13 +54,15 @@ class TangentMapTests(unittest.TestCase):
         """
 
         field = ReimanField()
-        tm = tangent_map(field, R0=1.210161051261806, periods=6, rtol=1e-12, atol=1e-12)
+        pfl = periodic_field_line(field, 31, periods=6, R0=1.2)
+        #tm = tangent_map(field, R0=1.210161051261806, periods=6, rtol=1e-12, atol=1e-12)
+        tm = tangent_map(field, pfl, rtol=1e-12, atol=1e-12)
         places = 10
         self.assertAlmostEqual(tm.iota_per_period, 0.02892783769228813, places=places)
         self.assertAlmostEqual(tm.iota, 0.02892783769228813, places=places)
         M = np.array([[ 0.983527264227946, -0.026329866716251],
                       [ 1.240952748303061, 0.983527264237678]])
-        np.testing.assert_allclose(tm.mat, M, atol=1.0e-6)
+        np.testing.assert_allclose(tm.full_orbit_tangent_maps[0], M, atol=1.0e-6)
         self.assertAlmostEqual(tm.residue, 0.008236367883594053, places=places)
         
     def test_CaryHanson1986_page2469(self):
@@ -68,7 +76,9 @@ class TangentMapTests(unittest.TestCase):
 
         # First consider the magnetic axis.
         # Cary & Hanson quote the axis position as 0.9822, but I get a bit higher, 0.9833
-        tm = tangent_map(field, R0=0.983393787292938)
+        pfl = periodic_field_line(field, 31, R0=1.0)
+        tm = tangent_map(field, pfl)
+        #tm = tangent_map(field, R0=0.983393787292938)
         places = 8
         # Cary & Hanson quote iota_per_period = 0.324. I get a bit lower.
         self.assertAlmostEqual(tm.iota_per_period, 0.3102585112979056, places=places)
